66672d96ad7ea7a73bd29f3c0648fddaf62d74d4
Jiri Vlasak <jirivlasak@gmail.com>
2024-04-10 20:18:47 +0200

    Added max supply and burn protection

--- contracts/ModifiedERC20.sol
+++ contracts/ModifiedERC20.sol
@@ -4,7 +4,48 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CustomToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("CustomToken", "CTK") {
-        _mint(msg.sender, initialSupply);
+    uint256 private _maxSupply;
+
+    constructor(uint256 maxSupply_) ERC20("CustomToken", "CTK") {
+        _maxSupply = maxSupply_;
+    }
+
+    modifier validRecipient(address _recipient) {
+        require(
+            _recipient != address(0),
+            "Cannot transfer to the zero address"
+        );
+        _;
+    }
+
+    modifier validSupply(uint256 _amount) {
+        require(
+            totalSupply() + _amount <= maxSupply(),
+            "Cannot mint more than max supply"
+        );
+        _;
+    }
+
+    function maxSupply() public view virtual returns (uint256) {
+        return _maxSupply;
+    }
+
+    function mint(address to, uint256 amount) public validSupply(amount) {
+        _mint(to, amount);
+    }
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public override validRecipient(recipient) returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override validRecipient(recipient) returns (bool) {
+        return super.transferFrom(sender, recipient, amount);
     }
 }
--- test/TestToken.js
+++ test/TestToken.js
@@ -3,17 +3,23 @@ const CustomToken = artifacts.require("CustomToken");
 contract("CustomToken", accounts =>
 {
     let token;
-    const initialSupply = 1000;
+    const initialSupply = 0;
 
     beforeEach(async () =>
     {
-        token = await CustomToken.new(initialSupply, { from: accounts[0] });
+        token = await CustomToken.new({ from: accounts[0] });
     });
 
     it("should put initialSupply amount of tokens in the first account", async () =>
     {
         const balance = await token.balanceOf(accounts[0]);
-        assert.equal(balance.valueOf(), initialSupply, "First account did not receive the initial supply");
+        assert.equal(balance.valueOf(), 0, "First account did not receive the initial supply");
+    });
+
+    it("should return the correct total supply after construction", async () =>
+    {
+        const totalSupply = await token.totalSupply();
+        assert.equal(totalSupply, initialSupply);
     });
 
     it("should be able to transfer tokens between accounts", async () =>
@@ -24,6 +30,44 @@ contract("CustomToken", accounts =>
         const receiverBalance = await token.balanceOf(accounts[1]);
         assert.equal(senderBalance.valueOf(), initialSupply - amount, "Amount wasn't correctly taken from the sender");
         assert.equal(receiverBalance.valueOf(), amount, "Amount wasn't correctly sent to the receiver");
+    });
+
+
+
+    it("should not allow to transfer more tokens than available in total", async () =>
+    {
+        try
+        {
+            await token.transfer(accounts[1], initialSupply + 1, { from: accounts[0] });
+        } catch (error)
+        {
+            assert(error.message.includes("revert"), "Expected revert error, got: " + error.message);
+            return;
+        }
+        assert.fail('Expected revert not received');
+    });
+
+    it("should not allow to transfer tokens from an account with insufficient balance", async () =>
+    {
+        try
+        {
+            await token.transferFrom(accounts[1], accounts[2], 1, { from: accounts[0] });
+        } catch (error)
+        {
+            assert(error.message.includes("revert"), "Expected revert error, got: " + error.message);
+            return;
+        }
+        assert.fail('Expected revert not received');
+    });
+
+    it("should update balances after transfers", async () =>
+    {
+        const amount = 100;
+        await token.transfer(accounts[1], amount, { from: accounts[0] });
+        const senderBalance = await token.balanceOf(accounts[0]);
+        const receiverBalance = await token.balanceOf(accounts[1]);
+        assert.equal(senderBalance, initialSupply - amount);
+        assert.equal(receiverBalance, amount);
     });
 
     // Add more tests as needed...
